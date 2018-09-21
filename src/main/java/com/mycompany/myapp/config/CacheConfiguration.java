205be483cd9e44c545a5b1017937e1f09956df77
Vladimir Saev <Vladimir_Saev@epam.com>
2018-09-21 17:36:08 +0300

    First commit

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -36,6 +36,18 @@ public class CacheConfiguration {
     @Bean
     public JCacheManagerCustomizer cacheManagerCustomizer() {
         return cm -> {
+            cm.createCache(com.mycompany.myapp.domain.Region.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Country.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Location.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Department.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Department.class.getName() + ".employees", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Task.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Task.class.getName() + ".jobs", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Employee.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Employee.class.getName() + ".jobs", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Job.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Job.class.getName() + ".tasks", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.JobHistory.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
